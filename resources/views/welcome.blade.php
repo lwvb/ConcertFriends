c9d34e0813630ab0cd1d56b26cd4c5372e4bddb9
Koding <pbredewold@pbredewold.koding.io>
2016-02-20 07:47:44 -0600

    Put a map on the homepage with leaflet

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,6 +3,8 @@
     <head>
         <title>ConcertFriends</title>
         <link href="/css/app.css" rel="stylesheet" type="text/css">
+        <script src="http://cdn.leafletjs.com/leaflet/v0.7.7/leaflet.js"></script>
+        <script src="/js/map.js" ></script>
     </head>
     <body>
         <div class="Site">
@@ -10,7 +12,7 @@
                 <h1 class="Header-title u-marginAn"> ConcertFriends </h1>
                 <p class="Header-description">Find someone to go with you to an awesome event</p>
             </div>
-            <div class="Event-map">
+            <div class="Event-map" id="map">
                 
             </div>
             <div class="Event-content Container">
@@ -45,6 +47,17 @@
         </div>
         
         @include('icons')
+        
+        <script>
+            var map = L.map('map').setView([52.162, 5.095], 9);
+    
+            L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+                attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+                maxZoom: 18,
+                id: 'pbredewold.p74hj116',
+                accessToken: 'pk.eyJ1IjoicGJyZWRld29sZCIsImEiOiJ6V2NGajNBIn0.37Amj6Jg93KOTbvtvPthog'
+            }).addTo(map);
+        </script>
         
     </body>
 </html>
